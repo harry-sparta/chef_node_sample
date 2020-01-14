29c95831dbf1e3c05e4fbc994cbce92e933f4580
harry-sparta <ka.hei.harry@gmail.com>
2020-01-14 15:52:44 +0000

    initial commit

--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,19 @@
 # Recipe:: default
 #
 # Copyright:: 2020, The Authors, All Rights Reserved.
+
+# imports of recipes
+include_recipe ['apt', 'nodejs']
+
+# packages apt-get
+apt_update
+package 'npm'
+package 'nginx'
+
+# npm install
+nodejs_npm 'pm2'
+
+# services
+service 'nginx' do
+  action [:start, :enable]
+end
